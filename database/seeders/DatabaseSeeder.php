48b0d11b8a820d674014d32e6ee52f35ac05b0b9
Chmel1 <besvlat@bk.ru>
2026-01-30 22:52:18 +0300

    Реализовал сидеры и фабрики

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Comment;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Article::factory(5)
+        ->has(Comment::factory()->count(3))
+        ->create();
     }
 }
